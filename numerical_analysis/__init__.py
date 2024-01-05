ab39a44739a71a1b09b70cd78bb5dfe5d2fc9c6f
John H. Freeman <john@freemania.pro>
2024-01-05 05:22:09 -0500

    Update __init__.py

--- numerical_analysis/__init__.py
+++ numerical_analysis/__init__.py
@@ -1,3 +1,5 @@
+# John H. Freeman © 2023
+
 __all__ = ['differentiation_integration', 'interpolation_polynomial', 'ivp_ode', 'linear_systems', 'nonlinear_systems', 'ode', 'one_variable' ]
 import differentiation_integration
 import interpolation_polynomial
