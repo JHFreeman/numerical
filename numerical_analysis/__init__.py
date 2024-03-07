94b5bdd935f92254e97b189cf714bd68f9bc2fe2
John H. Freeman <john@freemania.pro>
2024-03-06 22:31:15 -0500

    Freedom faighter patch 2 (#3)
    * Update __init__.py
    
    * Update one_variable.py
    
    updated with cited name for method of root finding
    
    * Update __init__.py

---
+++ numerical_analysis/__init__.py
@@ -0,0 +1,10 @@
+# John H. Freeman © 2023
+
+__all__ = ['differentiation_integration', 'interpolation_polynomial', 'ivp_ode', 'linear_systems', 'nonlinear_systems', 'ode', 'one_variable' ]
+import differentiation_integration
+import interpolation_polynomial
+import ivp_ode
+import linear_systems
+import nonlinear_systems
+import ode
+import one_variable
